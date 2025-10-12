2482a2fa1b0f11bdc33614127f8c0141bb262ecf
Marius Pardo <pardomarius@gmail.com>
2025-10-12 16:57:57 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -38,10 +38,6 @@ fn main() -> anyhow::Result<()> {
     let window_event_notifier = hooks::launch_window_hook().unwrap();
 
     for () in window_event_notifier {
-        // Extremely weird bug: some windows api calls doesn't work if this sleep is not executed.
-        // This is likely due to the threading context (the window event hook running in another thread for example), or not lol
-        thread::sleep(Duration::from_nanos(1));
-
         let windows_snapshot = opened_windows()?;
         info!(
             "Opened windows: {:#?}",
