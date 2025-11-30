ef437958794399b5e95d8d3dde87169e9832d2b8
sub07 <pardomarius@gmail.com>
2025-11-30 16:22:26 +0100

    Fix issue #22 (#23)
    * Fix issue #22
    
    * Fix warnings

--- src/system.rs
+++ src/system.rs
@@ -1,13 +1,7 @@
-use log::{error, warn};
+use log::warn;
 use windows::Win32::{
     Graphics::Gdi::{COLOR_HIGHLIGHT, GetSysColor},
-    UI::{
-        Input::KeyboardAndMouse::{
-            GetKeyState, VIRTUAL_KEY, VK_CONTROL, VK_LCONTROL, VK_LSHIFT, VK_LWIN, VK_MENU,
-            VK_RWIN, VK_SHIFT,
-        },
-        WindowsAndMessaging::{GetSystemMetrics, SM_CXSCREEN, SM_CYSCREEN},
-    },
+    UI::WindowsAndMessaging::{GetSystemMetrics, SM_CXSCREEN, SM_CYSCREEN},
 };
 
 use crate::{
@@ -26,42 +20,6 @@ pub fn screen_size() -> anyhow::Result<Size> {
 
 pub fn highlight_color() -> Color {
     Color::from_abgr_packed(unsafe { GetSysColor(COLOR_HIGHLIGHT) })
-}
-
-#[must_use]
-pub fn current_modifiers() -> keyboard_types::Modifiers {
-    fn is_vkey_down(key: VIRTUAL_KEY) -> bool {
-        let key_state = match wincall_into_result!(GetKeyState(key.0.into())) {
-            Ok(res) => u16::from_ne_bytes(res.to_ne_bytes()),
-            Err(e) => {
-                error!(
-                    "Error while checking if {key:?} modifier is pressed, defaulting to not pressed: {e}"
-                );
-                0
-            }
-        };
-        key_state & 0xFF80 == 0xFF80
-    }
-
-    let mut modifiers = keyboard_types::Modifiers::empty();
-
-    if is_vkey_down(VK_SHIFT) || is_vkey_down(VK_LSHIFT) {
-        modifiers.insert(keyboard_types::Modifiers::SHIFT);
-    }
-
-    if is_vkey_down(VK_CONTROL) || is_vkey_down(VK_LCONTROL) {
-        modifiers.insert(keyboard_types::Modifiers::CONTROL);
-    }
-
-    if is_vkey_down(VK_LWIN) || is_vkey_down(VK_RWIN) {
-        modifiers.insert(keyboard_types::Modifiers::META);
-    }
-
-    if is_vkey_down(VK_MENU) {
-        modifiers.insert(keyboard_types::Modifiers::ALT);
-    }
-
-    modifiers
 }
 
 pub fn restore_windows() {
