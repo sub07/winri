4eb1e260772d85e6457695378ca16688c4ec432e
Marius Pardo <pardomarius@gmail.com>
2025-11-30 16:16:20 +0100

    Fix issue #22

--- src/hook/key.rs
+++ src/hook/key.rs
@@ -5,6 +5,7 @@ use rdev::{EventType, Key};
 
 use crate::system;
 
+#[derive(Debug)]
 pub struct Event(pub Modifiers, pub Key);
 
 const fn is_meta_key(key: Key) -> bool {
@@ -16,7 +17,7 @@ pub fn launch_hook() -> Receiver<Event> {
     std::thread::Builder::new()
         .name("global-key-hook".into())
         .spawn(move || {
-            let mut meta_pressed = false;
+            let mut modifiers = Modifiers::default();
             rdev::_grab(move |event| {
                 if matches!(
                     event.event_type,
@@ -28,24 +29,53 @@ pub fn launch_hook() -> Receiver<Event> {
                     return Some(event);
                 }
 
-                let mut modifiers = system::current_modifiers();
-                modifiers.set(Modifiers::META, meta_pressed);
-
                 match event.event_type {
                     rdev::EventType::KeyPress(key) => {
-                        if is_meta_key(key) {
-                            meta_pressed = true;
-                            return None;
+                        match key {
+                            Key::ShiftLeft | Key::ShiftRight => {
+                                modifiers.set(Modifiers::SHIFT, true);
+                                return Some(event);
+                            }
+                            Key::ControlLeft | Key::ControlRight => {
+                                modifiers.set(Modifiers::CONTROL, true);
+                                return Some(event);
+                            }
+                            Key::Alt => {
+                                modifiers.set(Modifiers::ALT, true);
+                                return Some(event);
+                            }
+                            // Got an unknown key code for the right Meta key for some reason
+                            Key::MetaLeft | Key::MetaRight | Key::Unknown(92) => {
+                                modifiers.set(Modifiers::META, true);
+                                // Win key presses are swallowed to avoid opening the Start Menu, and triggering system shortcuts
+                                // Winri is supposed to be a sort of "command center" for the system, so the native system shortcuts should not be needed
+                                // I know this might be controversial, but it's the intended behavior for now, and I'm open to feedback on this matter
+                                return None;
+                            }
+                            _ => {
+                                sender.send(Event(modifiers, key)).unwrap();
+                                return (!modifiers.contains(Modifiers::META)).then_some(event);
+                            }
                         }
-                        sender.send(Event(modifiers, key)).unwrap();
-                        return (!meta_pressed).then_some(event);
                     }
                     rdev::EventType::KeyRelease(key) => {
-                        if is_meta_key(key) {
-                            meta_pressed = false;
-                            return None;
+                        match key {
+                            Key::ShiftLeft | Key::ShiftRight => {
+                                modifiers.set(Modifiers::SHIFT, false);
+                            }
+                            Key::ControlLeft | Key::ControlRight => {
+                                modifiers.set(Modifiers::CONTROL, false);
+                            }
+                            Key::Alt => {
+                                modifiers.set(Modifiers::ALT, false);
+                            }
+                            // Got an unknown key code for the right Meta key for some reason
+                            Key::MetaLeft | Key::MetaRight | Key::Unknown(92) => {
+                                modifiers.set(Modifiers::META, false);
+                                return None;
+                            }
+                            _ => {}
                         }
-                        return (!meta_pressed).then_some(event);
                     }
                     _ => {}
                 }
